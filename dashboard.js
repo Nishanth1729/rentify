7b4f20c0696d47ebd5cf705f9caf30d3fa160fbc
Nishanth1729 <161317837+Nishanth1729@users.noreply.github.com>
2024-05-18 12:21:32 +0530

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropertyForm from '../components/PropertyForm';
+import PropertyList from '../components/PropertyList';
+
+const Dashboard = () => (
+    <div>
+        <h1>Dashboard</h1>
+        <PropertyForm />
+        <PropertyList />
+    </div>
+);
+
+export default Dashboard;
